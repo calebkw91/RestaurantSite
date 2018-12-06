50f4adcf9728efbf4508679a8e0b6f1a0d103509
Caleb Walker <walke1291@gmail.com>
2018-12-06 14:27:04 -0800

    part_3_rev_2

---
+++ scripts/restaurantSite.js
@@ -0,0 +1,28 @@
+function formSubmit(){
+    var contactName = document.forms["contactForm"]["name"].value;
+    var contactEmail = document.forms["contactForm"]["email"].value;
+    var contactPhone = document.forms["contactForm"]["phone"].value;
+    
+    
+    if(contactEmail == ""){
+        alert("Email must be filled in.");
+        document.forms["contactForm"]["email"].focus();
+        return false;
+    }
+    
+    if(contactName == ""){
+        alert("Name must be filled in.");
+        document.forms["contactForm"]["name"].focus();
+        return false;
+    }
+    
+    if(contactPhone == ""){
+        alert("Phone number must be filled in.");
+        document.forms["contactForm"]["phone"].focus();
+        return false;
+    }
+    
+    alert("Information Submitted!");
+    
+    return false;
+}
